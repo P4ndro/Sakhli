b9ed60d474b5c70235f8038e23430594915a670d
Juli CHaphidze <chapidzejuli02@gmail.com>
2025-12-30 23:58:13 +0400

    herocard-half

---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,21 @@
+import "./homePage.scss";
+
+function HomePage() {
+    return (
+        <div className="homePage">
+           <div className="textContainer">
+            <div className="wrapper">
+            <h1 className="title">Find Your Sakhli & Make Your Dream Come True
+                 </h1>
+                 <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+                 </p>
+            </div>
+            </div>
+           <div className="imageContainer">
+            <img src="bg.png" alt="home" />
+           </div>
+        </div>
+    )
+}
+export default HomePage;
