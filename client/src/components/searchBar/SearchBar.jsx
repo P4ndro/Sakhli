57df782179caba1ad9e72c47006738bc7adae6a4
Juli CHaphidze <chapidzejuli02@gmail.com>
2025-12-31 19:21:02 +0400

    homepage-done

---
+++ client/src/components/searchBar/SearchBar.jsx
@@ -0,0 +1,54 @@
+import "./SearchBar.scss";
+import { useState } from "react";
+
+const types = ["Buy", "Rent"];
+
+function SearchBar() {
+    const [query, setQuery] = useState({
+      type: "Buy",
+      location: "",
+      minPrice: 0,
+      maxPrice: 1000000,
+    });
+
+    const switchType = (val) => {
+      setQuery((prev) => ({ ...prev, type: val }));
+    };
+    return (
+        <div className="searchBar">
+           <div className="type">
+            {types.map(type => (
+            <button key={type} onClick={() => switchType(type)} className={query.type === type ? "active" : ""}>
+              {type}
+              </button>
+            ))}
+ 
+           </div>
+           <form>
+            <input type = "text" 
+            name="location"
+             placeholder="City Location" 
+             />
+            <input type = "number"
+             name="minPrice"
+              min={0}
+               max={1000000} 
+              placeholder="Min Price" 
+              />
+            <input type = "number" 
+            name="maxPrice"
+             min={0} 
+             max={1000000} 
+             placeholder="Max Price"
+              />
+            
+            <button>
+                <img src ="/search.png" alt="search"    />
+            </button>
+        
+          </form>
+           
+        </div>
+    );
+}
+export default SearchBar;
--- client/src/routes/homePage/homePage.jsx
+++ client/src/routes/homePage/homePage.jsx
@@ -1,4 +1,5 @@
 import "./homePage.scss";
+import SearchBar from "../../components/searchBar/SearchBar";
 
 function HomePage() {
     return (
@@ -10,6 +11,21 @@ function HomePage() {
                  <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
                  </p>
+                 <SearchBar />
+                 <div className="boxes">
+                        <div className="box">
+                            <h1>20+</h1>
+                            <h2>Years of Experience</h2>
+                        </div>
+                        <div className="box">
+                            <h1>200</h1>
+                            <h2>Awards Winning</h2>
+                        </div>
+                        <div className="box">
+                            <h1>209900</h1>
+                            <h2>Property Ready</h2>
+                        </div>
+                 </div>
             </div>
             </div>
            <div className="imageContainer">
