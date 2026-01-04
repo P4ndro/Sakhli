953905966edf91f4cc21b27b01c9c164bf67f0a0
Juli CHaphidze <chapidzejuli02@gmail.com>
2026-01-04 19:54:35 +0400

    lil-changes

--- client/src/components/filter/filter.jsx
+++ client/src/components/filter/filter.jsx
@@ -10,7 +10,30 @@ function Filter() {
             </div>
         </div>
             <div className="bottom">
-                <div className="item"></div>
+                <div className="item">
+                <label htmlFor="type">type</label>
+                <select name="type" id= "Type">
+                    <option value="Buy">Buy</option>
+                    <option value="Rent">Rent</option>
+                </select>
+                <div className="item">
+                    <label  htmlfor="property">Property</label>
+                    <select name="property" id= "property">
+                        <option value="apartment">Apartment</option>
+                        <option value="house">House</option>
+                        <option value="condo">Condo</option>
+                        <option value="land">Land</option>
+                    </select>
+                </div>
+                <div className="item">
+                    <label htmlfor = "minPrice">Min Price</label>
+                    <input type="number">
+                    </input>
+                </div>
+                <button>
+                    <img src="/.search.png" alt= "srch"/>                       
+                </button>
+                </div>
             </div>
         </div>
     )
