b7f828e3e10bf9ddefaffcfdd3c5551bf3db5375
Juli CHaphidze <chapidzejuli02@gmail.com>
2026-01-10 23:40:59 +0400

    map- done

---
+++ client/src/components/pin/pin.jsx
@@ -0,0 +1,23 @@
+import { Marker, Popup } from "react-leaflet";
+import "./pin.scss";
+import { Link } from "react-router-dom";
+
+
+function Pin({item}) {
+
+    return (
+        <Marker position={[item.latitude, item.longitude]}>
+        <Popup>
+            <div className="popupContainer">
+                <img src={item.img} alt="popup" />
+                <div className="textContainer"> 
+                    <Link to={`/${item.id}`} >{item.title}</Link>
+                    <span >{item.bedroom} bedrooms</span>
+                    <b>${item.price}</b>
+                </div>
+            </div>
+        </Popup>
+    </Marker>
+    )
+}
+export default Pin;
